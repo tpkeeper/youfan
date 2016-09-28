6b9f62376f8d8edd48d69e58949438b7c4039cba
tpkeeper <1056883354@qq.com>
2016-09-28 13:53:21 +0800

    引入bgabanner 实现轮播图

--- app/src/main/java/com/tk/youfan/adapter/home/HomeRecyclerViewAdapter.java
+++ app/src/main/java/com/tk/youfan/adapter/home/HomeRecyclerViewAdapter.java
@@ -1,9 +1,12 @@
 package com.tk.youfan.adapter.home;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.tk.youfan.R;
 import com.tk.youfan.domain.home.Module;
 
 import java.util.List;
@@ -16,14 +19,74 @@ import java.util.List;
  */
 public class HomeRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     List<Module> moduleList;
-
-    public HomeRecyclerViewAdapter(List<Module> moduleList) {
+    Context mContext;
+    public HomeRecyclerViewAdapter(List<Module> moduleList,Context mContext) {
         this.moduleList = moduleList;
+        this.mContext = mContext;
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = null;
+        switch (viewType) {
+            case 1:
+        view = LayoutInflater.from(mContext).inflate(R.layout.top_img_module,parent,false);
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+            case 7:
+
+                break;
+            case 8:
+
+                break;
+            case 9:
+
+                break;
+            case 10:
+
+                break;
+            case 11:
+
+                break;
+            case 12:
+
+                break;
+            case 13:
+
+                break;
+            case 14:
+
+                break;
+            case 15:
+
+                break;
+            case 16:
+
+                break;
+            case 17:
+
+                break;
+            case 18:
+
+                break;
+            case 19:
+
+                break;
+        }
         return new TopImgModuleHolder(view);
     }
 
@@ -39,8 +102,7 @@ public class HomeRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.V
 
     @Override
     public int getItemViewType(int position) {
-
-        return super.getItemViewType(position);
+        return moduleList.get(position).getId();
     }
 }
 
